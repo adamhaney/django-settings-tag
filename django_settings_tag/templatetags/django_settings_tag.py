523d25489621b12dca2040f44b3e3cd350bbb2c9
Adam Haney <adamhaney@users.noreply.github.com>
2015-11-29 15:15:12 -0500

    Most recent version of django has problems importing settings at load time

--- django_settings_tag/templatetags/django_settings_tag.py
+++ django_settings_tag/templatetags/django_settings_tag.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django import template
-from django.conf import settings
 
 register = template.Library()
 
 
 @register.simple_tag(name='settings')
 def settings(attr):
+    from django.conf import settings
     return getattr(settings, attr)
